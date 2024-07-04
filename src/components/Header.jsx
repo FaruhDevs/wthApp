1b9af05411ab6b5e8d87ab379ed1f3e3c283f1d6
FaruhDevs <faruh.hudaykuliyew@mail.ru>
2024-07-04 15:33:38 +0300

    aa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
     <div className='flex flex-row items-center  justify-between pt-3 px-1 custom-sm:px-4 gap-5'>
       <img className='w-24 h-4 md:w-36 md:h-7' src={imageSrc} />
       <p className='text-slate-300 text-xs md:text-sm font-semibold'>{getFormattedDate()}</p>
-      <a href="https://github.com/your-github-username" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/FaruhDevs/wthApp" target="_blank" rel="noopener noreferrer">
         <i className="fa-brands fa-github md:text-2xl text-xl text-white"></i>
       </a>
     </div>
